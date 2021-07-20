c39ab81c5ad6ae61faf2af162cf7bda8ffd065eb
felix-mohrmann <mohrmann.felix@gmail.com>
2021-07-20 14:59:43 +0200

    Add add and remove method

--- src/main/java/StudentDB.java
+++ src/main/java/StudentDB.java
@@ -12,6 +12,7 @@ public class StudentDB {
         newStudent[1] = student2;
         newStudent[2] = student3;
 
+
     }
 
     private Student[] studentArray;
@@ -21,6 +22,7 @@ public class StudentDB {
     }
 
     public Student[] list() {
+        //ist gleich mit getter Methode für Arrays und gibt studentArray zurück
         return studentArray;
     }
 
@@ -38,5 +40,27 @@ public class StudentDB {
         int randomNumber = (int)Math.random() * studentArray.length;
 
         return studentArray[randomNumber];
+    }
+
+    public void add(Student newStudent){
+        Student[] newStudentArray = new Student[studentArray.length + 1];
+        for (int i = 0; i < studentArray.length; i++) {
+            newStudentArray[i] = studentArray[i];
+        }
+        newStudentArray[studentArray.length] = newStudent;
+        studentArray = newStudentArray;
+    }
+
+    public void remove(String name) {
+        Student[] newStudentArray = new Student[studentArray.length -1];
+        int counter = 0;
+        for (int i = 0; i < studentArray.length; i++) {
+            if (studentArray[i].getName().equals(name)){
+                continue;
+            }
+            newStudentArray[counter] = studentArray[i];
+            counter ++;
+        }
+        studentArray = newStudentArray;
     }
 }
--- src/test/java/StudentDBTest.java
+++ src/test/java/StudentDBTest.java
@@ -36,8 +36,45 @@ class StudentDBTest {
         String actual = testDB.toString();
 
         // then
-        assertEquals("Peter 123\nPeter 123\nPeter 123\n" , actual);
+        assertEquals("Peter 123\nPeter 123\nPeter 123\n", actual);
     }
 
+    @Test
+    @DisplayName("check if add method works")
+    public void checkAdd() {
+        // given
+        Student[] studentArray = new Student[3];
+        studentArray[0] = new Student("Peter", 123);
+        studentArray[1] = new Student("Peter", 123);
+        studentArray[2] = new Student("Peter", 123);
+        StudentDB testDB = new StudentDB(studentArray);
+        assertEquals(testDB.list().length, 3);
 
+        // when
+        testDB.add(new Student("Klaus", 456));
+
+        // then
+        assertEquals(testDB.list().length, 4);
+    }
+
+    @Test
+    @DisplayName("check if remove method works")
+    public void checkRemove() {
+        // given
+        Student[] studentArray = new Student[3];
+        studentArray[0] = new Student("Peter", 123);
+        studentArray[1] = new Student("Klaus", 123);
+        studentArray[2] = new Student("Max", 123);
+        StudentDB testDB = new StudentDB(studentArray);
+        assertEquals(testDB.list().length, 3);
+        Student[] expected = {studentArray[0], studentArray[2]};
+
+        // when
+        testDB.remove("Klaus");
+        Student[] actual = testDB.list();
+
+        // then
+        assertEquals(2, actual.length);
+        assertArrayEquals(expected, actual);
+    }
 }
