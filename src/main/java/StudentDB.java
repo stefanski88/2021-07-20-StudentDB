95a286eec2ba10ca6c48b86fae61de636fe9e2ac
stefanbauer <alternative2k@googlemail.com>
2021-07-20 11:51:01 +0200

    StudentDB #2

--- src/main/java/StudentDB.java
+++ src/main/java/StudentDB.java
@@ -1,5 +1,19 @@
 public class StudentDB {
 
+    public static void main(String[] args) {
+
+        Student student1 = new Student("Lars", 321);
+        Student student2 = new Student("Peter", 341);
+        Student student3 = new Student("Michael", 999);
+
+        Student[] newStudent = new Student[3];
+
+        newStudent[0] = student1;
+        newStudent[1] = student2;
+        newStudent[2] = student3;
+
+    }
+
     private Student[] studentArray;
 
     public StudentDB(Student[] studentArray) {
@@ -20,9 +34,9 @@ public class StudentDB {
     }
 
     public Student randomStudent() {
-        studentArray[Math.random()]
 
-        Math.random();
+        int randomNumber = (int)Math.random() * studentArray.length;
 
+        return studentArray[randomNumber];
     }
 }
